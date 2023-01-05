9a0d8a76a743bcd3ab43e316175c4f47361188e1
Attituding <84680760+Attituding@users.noreply.github.com>
2023-01-04 19:35:23 -0800

    chore: init

---
+++ build.gradle.kts
@@ -0,0 +1,81 @@
+plugins {
+    idea
+    java
+    id("gg.essential.loom") version "0.10.0.+"
+    id("dev.architectury.architectury-pack200") version "0.1.3"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
+}
+
+val forgeVersion: String by project
+val mcpMappings: String by project
+val modGroup: String by project
+val modId: String by project
+val modVersion: String by project
+val minecraftVersion: String by project
+
+group = modGroup
+version = modVersion
+
+// Toolchains:
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(8))
+}
+
+// Minecraft configuration:
+loom {
+    forge {
+        pack200Provider.set(dev.architectury.pack200.java.Pack200Adapter())
+    }
+}
+
+sourceSets.main {
+    output.setResourcesDir(file("$buildDir/classes/java/main"))
+}
+
+// Dependencies:
+repositories {
+    mavenCentral()
+    maven("https://repo.spongepowered.org/maven/")
+}
+
+val shadowImpl: Configuration by configurations.creating {
+    configurations.implementation.get().extendsFrom(this)
+}
+
+dependencies {
+    minecraft("com.mojang:minecraft:${minecraftVersion}")
+    mappings("de.oceanlabs.mcp:mcp_stable:${mcpMappings}")
+    forge("net.minecraftforge:forge:${forgeVersion}")
+}
+
+// Tasks:
+tasks.withType(JavaCompile::class) {
+    options.encoding = "UTF-8"
+}
+
+tasks.withType(Jar::class) {
+    archiveBaseName.set(modId)
+    manifest.attributes.run {
+        this["FMLCorePluginContainsFMLMod"] = "true"
+        this["ForceLoadAsMod"] = "true"
+    }
+}
+
+
+val remapJar by tasks.named<net.fabricmc.loom.task.RemapJarTask>("remapJar") {
+    archiveClassifier.set("all")
+    from(tasks.shadowJar)
+    input.set(tasks.shadowJar.get().archiveFile)
+}
+
+tasks.shadowJar {
+    archiveClassifier.set("all-dev")
+    configurations = listOf(shadowImpl)
+    doLast {
+        configurations.forEach {
+            println("Config: ${it.files}")
+        }
+    }
+}
+
+tasks.assemble.get().dependsOn(tasks.remapJar)
--- src/main/java/xyz/attituding/notifhy/NotifHy.java
+++ src/main/java/xyz/attituding/notifhy/NotifHy.java
@@ -1,105 +1,13 @@
 package xyz.attituding.notifhy;
 
-import com.google.common.net.InternetDomainName;
-import com.google.gson.JsonObject;
-import me.shedaniel.autoconfig.AutoConfig;
-import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
-import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.network.ClientPlayNetworkHandler;
-import org.apache.http.HttpResponse;
-import org.apache.http.HttpStatus;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import xyz.attituding.notifhy.config.NotifHyConfig;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
-import java.net.InetSocketAddress;
-import java.net.SocketAddress;
-import java.util.Base64;
-
-public class NotifHy implements ClientModInitializer {
-    public static final Logger LOGGER = LoggerFactory.getLogger("NotifHy");
-    public static final String MOD_ID = "notifhy";
-
-    @Override
-    public void onInitializeClient() {
-        AutoConfig.register(NotifHyConfig.class, GsonConfigSerializer::new);
-
-        ClientPlayConnectionEvents.JOIN.register((handler, sender, client) -> {
-            JsonObject json = new JsonObject();
-            json.addProperty("joined", true);
-            preconditions(json, handler);
-        });
-
-        ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
-            JsonObject json = new JsonObject();
-            json.addProperty("joined", false);
-            preconditions(json, handler);
-        });
-    }
-
-    private static void preconditions(JsonObject json, ClientPlayNetworkHandler handler) {
-        NotifHyConfig config = AutoConfig.getConfigHolder(NotifHyConfig.class).getConfig();
-
-        if (config.authentication.length() == 0) {
-            LOGGER.warn("No authentication token set");
-            return;
-        }
-
-        SocketAddress socketAddress = handler.getConnection().getAddress();
-
-        // Verify SocketAddress is InetSocketAddress
-        // CC BY-SA 3.0 https://stackoverflow.com/a/22691011
-        if (!(socketAddress instanceof InetSocketAddress inetSocketAddress)) {
-            LOGGER.warn("Socket address is not an internet protocol socket, might be a local world: " + socketAddress.toString());
-            return;
-        }
-
-        String hostString = inetSocketAddress.getHostString();
-        String domain = InternetDomainName.from(hostString).topPrivateDomain().toString();
-
-        // Ignore all domains that are not in the list (modifiable in config)
-        if (!config.advanced.domains.contains(domain)) {
-            LOGGER.warn("Private domain is not in list: " + domain);
-            return;
-        }
-
-        json.addProperty("domain", domain);
-
-        ping(json);
-    }
-
-    public static void ping(JsonObject json) {
-        try {
-            NotifHyConfig config = AutoConfig.getConfigHolder(NotifHyConfig.class).getConfig();
-
-            String uuid = MinecraftClient.getInstance().getSession().getUuid();
-            String authorization = "Basic " + Base64.getEncoder().encodeToString((uuid + ":" + config.authentication).getBytes());
-
-            HttpClient httpClient = HttpClientBuilder.create().build();
-            HttpPost request = new HttpPost(config.advanced.server);
-            request.addHeader("Context-Type", "application/json");
-            request.addHeader("Authorization", authorization);
-            request.setEntity(new StringEntity(json.toString()));
-
-            HttpResponse response = httpClient.execute(request);
-
-            LOGGER.debug("Sending " + json);
-
-            int responseCode = response.getStatusLine().getStatusCode();
-
-            if (responseCode == HttpStatus.SC_OK) {
-                LOGGER.debug("Successfully pinged");
-            } else {
-                LOGGER.warn("Failed to ping with response code " + responseCode);
-            }
-        } catch (Exception e) {
-            LOGGER.error("Failed to ping", e);
-        }
+@Mod(modid = "notifhy", version = "0.0.1")
+public class NotifHy {
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        System.out.println("Dirt: " + Blocks.dirt.getUnlocalizedName());
     }
 }
